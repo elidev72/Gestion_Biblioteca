3e361ec48633443d59c9f8e1a4f80ee9ec4a57bd
EliasRamil <raeli0072@gmail.com>
2025-01-28 18:29:42 -0300

    Correciones diagrama

--- src/models/prestamo.py
+++ src/models/prestamo.py
@@ -6,8 +6,9 @@ class Prestamo(db.Model):
     fecha_fin = db.Column(db.Date, nullable=False)
     entregado = db.Column(db.Boolean, default=False)
     cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'), nullable=False)
-    libro_id = db.Column(db.Integer, db.ForeignKey('libro.id'), nullable=False)
+    libro_id = db.Column(db.Integer, db.ForeignKey('libro_stock.id'), nullable=False)
+    bibliotecario_id = db.Column(db.Integer, db.ForeignKey('bibliotecario.id'), nullable=False)
     
     def __repr__(self):
-        return f"Prestamo(id='{self.id}', fecha_inicio='{self.fecha_inicio}', fecha_fin='{self.fecha_fin}', entregado='{self.entregado}', cliente_id='{self.cliente_id}', libro_id='{self.libro_id}')"
+        return f"Prestamo(id='{self.id}', fecha_inicio='{self.fecha_inicio}', fecha_fin='{self.fecha_fin}', entregado='{self.entregado}', cliente_id='{self.cliente_id}', libro_id='{self.libro_id}', bibliotecario_id='{self.bibliotecario_id}')"
     
